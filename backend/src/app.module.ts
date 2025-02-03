d1d60a70f22c8ab4470139a4cfd29c1069c409c4
nossbigg <chenggibson@gmail.com>
2025-02-03 15:16:09 +0800

    Init /backend repo

---
+++ backend/src/app.module.ts
@@ -0,0 +1,11 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { ResourcesModule } from './resources/resources.module';
+
+@Module({
+  imports: [ResourcesModule],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
