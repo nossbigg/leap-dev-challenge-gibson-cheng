c44cef961838a0ac1a94b2cc7537708ac171c456
nossbigg <chenggibson@gmail.com>
2025-02-05 00:17:00 +0800

    Fix stale UI data on prompt update issue

--- frontend/src/components/PromptCard/PromptCard.tsx
+++ frontend/src/components/PromptCard/PromptCard.tsx
@@ -4,7 +4,6 @@ import { useCallback } from "react";
 import { EditableTextBox } from "../EditableTextBox";
 import { Prompt } from "@/api/prompts/prompts.types";
 import { updatePrompt } from "@/api/prompts/prompts.api";
-import { useRouter } from "next/navigation";
 
 interface Props {
   prompt: Prompt;
@@ -13,15 +12,14 @@ export const PromptCard: React.FC<Props> = (props) => {
   const { prompt } = props;
   const { content, id } = prompt;
 
-  const router = useRouter();
-
   const onSave = useCallback(
     async (newValue: string) => {
       await updatePrompt(id, newValue);
-      router.refresh();
+      // note: shoudl use router.refresh() here, but there's issues with server/client state reconciliation
+      window.location.reload();
       return true;
     },
-    [id, router]
+    [id]
   );
 
   return <EditableTextBox content={content} onSave={onSave} />;
