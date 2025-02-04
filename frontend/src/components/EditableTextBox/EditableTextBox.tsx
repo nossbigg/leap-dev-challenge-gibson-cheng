c74e68fc263cb418ae912f780448a83ef2ad054e
nossbigg <chenggibson@gmail.com>
2025-02-04 23:35:30 +0800

    Extract EditableTextBox

---
+++ frontend/src/components/EditableTextBox/EditableTextBox.tsx
@@ -0,0 +1,75 @@
+"use client";
+
+import Card from "@mui/material/Card";
+import styles from "./EditableTextBox.module.css";
+import { IconButton, TextField } from "@mui/material";
+
+import CloseIcon from "@mui/icons-material/Close";
+import DoneIcon from "@mui/icons-material/Done";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import { useCallback, useState } from "react";
+
+interface Props {
+  content: string;
+  onSave: (v: string) => Promise<boolean>;
+  onDelete: () => void;
+}
+export const EditableTextBox: React.FC<Props> = (props) => {
+  const { content, onSave, onDelete } = props;
+
+  const [isEditMode, setEditMode] = useState(false);
+  const [editedContent, setEditedContent] = useState(content);
+
+  const onEditEnable = () => setEditMode(true);
+  const onEditDisable = () => {
+    setEditMode(false);
+    setEditedContent(content);
+  };
+
+  const onSaveHandler = useCallback(async () => {
+    const r = await onSave(editedContent);
+    if (!r) {
+      return;
+    }
+
+    setEditMode(false);
+  }, [editedContent, onSave]);
+
+  if (isEditMode) {
+    return (
+      <Card className={styles.card}>
+        <div className={styles.cardContents}>
+          <TextField
+            value={editedContent}
+            onChange={(e) => setEditedContent(e.target.value)}
+            fullWidth
+            multiline
+          />
+        </div>
+        <div className={styles.cardControls}>
+          <IconButton onClick={onEditDisable}>
+            <CloseIcon />
+          </IconButton>
+          <IconButton onClick={onSaveHandler}>
+            <DoneIcon />
+          </IconButton>
+        </div>
+      </Card>
+    );
+  }
+
+  return (
+    <Card className={styles.card}>
+      <div className={styles.cardContents}>{content}</div>
+      <div className={styles.cardControls}>
+        <IconButton onClick={onEditEnable}>
+          <EditIcon />
+        </IconButton>
+        <IconButton onClick={onDelete}>
+          <DeleteIcon />
+        </IconButton>
+      </div>
+    </Card>
+  );
+};
--- frontend/src/components/PromptResponseCard/PromptResponseCard.tsx
+++ frontend/src/components/PromptResponseCard/PromptResponseCard.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 import { PromptResponse } from "@/api/promptResponses/promptResponses.types";
-import Card from "@mui/material/Card";
-import styles from "./PromptResponseCard.module.css";
-import { IconButton, TextField } from "@mui/material";
-
-import CloseIcon from "@mui/icons-material/Close";
-import DoneIcon from "@mui/icons-material/Done";
-import EditIcon from "@mui/icons-material/Edit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { useCallback, useState } from "react";
+import { useCallback } from "react";
 import {
   deletePromptResponse,
   updatePromptResponse,
 } from "@/api/promptResponses/promptResponses.api";
+import { EditableTextBox } from "../EditableTextBox";
 
 interface Props {
   promptResponse: PromptResponse;
@@ -24,61 +17,22 @@ export const PromptResponseCard: React.FC<Props> = (props) => {
   const { promptResponse, onDeleteSuccess, onUpdateSuccess } = props;
   const { content, id } = promptResponse;
 
-  const [isEditMode, setEditMode] = useState(false);
-  const [editedContent, setEditedContent] = useState(content);
-
-  const onEditEnable = () => setEditMode(true);
-  const onEditDisable = () => {
-    setEditMode(false);
-    setEditedContent(content);
-  };
-
   const onDelete = useCallback(async () => {
     await deletePromptResponse(id);
     alert("Prompt response deleted");
     onDeleteSuccess(id);
   }, [id, onDeleteSuccess]);
 
-  const onSave = useCallback(async () => {
-    await updatePromptResponse(id, editedContent);
-    onUpdateSuccess({ ...promptResponse, content: editedContent });
-    setEditMode(false);
-  }, [editedContent, id, onUpdateSuccess, promptResponse]);
-
-  if (isEditMode) {
-    return (
-      <Card className={styles.card}>
-        <div className={styles.cardContents}>
-          <TextField
-            value={editedContent}
-            onChange={(e) => setEditedContent(e.target.value)}
-            fullWidth
-            multiline
-          />
-        </div>
-        <div className={styles.cardControls}>
-          <IconButton onClick={onEditDisable}>
-            <CloseIcon />
-          </IconButton>
-          <IconButton onClick={onSave}>
-            <DoneIcon />
-          </IconButton>
-        </div>
-      </Card>
-    );
-  }
+  const onSave = useCallback(
+    async (newValue: string) => {
+      await updatePromptResponse(id, newValue);
+      onUpdateSuccess({ ...promptResponse, content: newValue });
+      return true;
+    },
+    [id, onUpdateSuccess, promptResponse]
+  );
 
   return (
-    <Card className={styles.card}>
-      <div className={styles.cardContents}>{content}</div>
-      <div className={styles.cardControls}>
-        <IconButton onClick={onEditEnable}>
-          <EditIcon />
-        </IconButton>
-        <IconButton onClick={onDelete}>
-          <DeleteIcon />
-        </IconButton>
-      </div>
-    </Card>
+    <EditableTextBox content={content} onDelete={onDelete} onSave={onSave} />
   );
 };
