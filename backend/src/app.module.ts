4a8315d0255c33b3a9643dee8c5e97d6d5e7d547
nossbigg <chenggibson@gmail.com>
2025-02-05 15:00:26 +0800

    Use .env for backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { PromptsController } from './resources/prompts/prompts.controller';
 import { PromptResponsesController } from './resources/promptResponses/promptResponses.controller';
 
 @Module({
   controllers: [PromptsController, PromptResponsesController],
+  imports: [ConfigModule.forRoot()],
 })
 export class AppModule {}
